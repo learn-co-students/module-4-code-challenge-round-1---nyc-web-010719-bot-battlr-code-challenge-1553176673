2b286bdb1f28933eab785d2d2494a8e87437e578
cvolpe12 <christopher.volpe12@gmail.com>
2019-03-21 10:35:08 -0400

    half way there

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,11 +4,17 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+  renderBots = () => {
+    return this.props.botArmy.map(bot => {
+      return <BotCard key={bot.id} bot={bot} addBotToArmy={this.props.addBotToArmy} parent={"BC"}/>
+    })
+  }
+
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.renderBots()}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,56 @@
 import React from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
+const API = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+
+  state = {
+    botArmy: [],
+    myArmy: []
+  }
+
+  addBotToArmy = (e, bot) => {
+    e.preventDefault()
+    if (this.state.myArmy.includes(bot)) {
+      null
+    } else {
+      this.setState((prevState)=>({
+        ...this.state,
+        myArmy: [...prevState.myArmy, bot]
+      }))
+    }
+  }
+
+  removeFromArmy = (e, bot) => {
+    e.preventDefault()
+    let index = this.state.myArmy.indexOf(bot)
+    this.state.myArmy.splice(index, 1)
+    let army = this.state.myArmy
+    this.setState((prevState)=>({
+      ...this.state,
+      myArmy: army
+    }))
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(r=>r.json())
+      .then(bots => {
+        this.setState({
+          ...this.state,
+          botArmy: bots
+        })
+      })
+  }
 
   render() {
     return (
       <div>
         {/* put your components here */}
+        <YourBotArmy myArmy={this.state.myArmy} removeFromArmy={this.removeFromArmy}/>
+        <BotCollection botArmy={this.state.botArmy} addBotToArmy={this.addBotToArmy}/>
       </div>
     );
   }
