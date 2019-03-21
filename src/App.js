34152ae59622c67b6260315f9bd2922e93afeb75
shhyatt <shhyatt@gmail.com>
2019-03-21 11:22:01 -0400

    almost finished

--- src/App.js
+++ src/App.js
@@ -3,10 +3,74 @@ import BotsPage from "./containers/BotsPage";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    bots: [],
+    army: []
+  }
+
+  handleClick = id => {
+    //console.log("HELLLOOOOOO", id);
+    this.findBot(id)
+  }
+  findBot = id => {
+    //console.log(id);
+    this.state.bots.find(bot => {
+      //console.log(bot);
+      if(bot.id === id){
+        let foundBot = bot
+        //console.log(foundBot);
+
+        let addToArmy = [...this.state.army, foundBot]
+        this.setState({
+          army: addToArmy
+
+        })
+      }
+    })
+  }
+
+  removeFromArmy = (id) => {
+    //console.log("Hello!!!!!!", id);
+    this.state.army.find(bot => {
+      if(bot.id === id){
+        let removedBot = bot
+        let index = this.state.army.indexOf(removedBot)
+        let newArmy = this.state.army
+        let deleteBot = newArmy.splice(index, 1)
+        this.setState({
+          army: newArmy
+        })
+        //console.log(index);
+        //console.log(removedBot);
+      }
+    })
+  }
+
+
+  fetchBots = () => {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+    .then(r => r.json())
+    .then(data => {
+      this.setState({
+        bots: data
+      })
+      //console.log(data);
+    })
+  }
+
+  componentDidMount = () => {
+    this.fetchBots()
+  }
   render() {
+    //console.log(this.state.bots);
+    //console.log(this.state.army);
     return (
       <div className="App">
-        <BotsPage />
+        <BotsPage
+        bots={this.state.bots}
+        army={this.state.army}
+        handleClick={this.handleClick}
+        removeFromArmy={this.removeFromArmy}/>
       </div>
     );
   }
--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,16 +5,19 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+    //console.log(this.props.bots);
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {<BotCard
+            bot={this.props.bot}
+            handleClick={this.props.handleClick}/>}
+
     		</div>
   	  </div>
   	);
   }
 
-};
+}
 
 export default BotCollection;
